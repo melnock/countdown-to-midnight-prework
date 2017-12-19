cdc6b7cbef32d85aa37a148ed0f12b15f115b90b
Melissa Nock <melnock@gmail.com>
2017-12-19 16:14:09 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@
 
 def countdown(number)
   while number > 0 
-    puts "#{number} SECOND(S) !"
+    puts "#{number} SECOND(S)!"
     number-= 1 
   end
  "HAPPY NEW YEAR!"
@@ -10,7 +10,7 @@ end
 
 def countdown_with_sleep (number)
   while number > 0 
-    "#{number} SECOND(S) !"
+    "#{number} SECOND(S)!"
     sleep 1 
     number -= 1 
 end 
